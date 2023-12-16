2c1c46b398367562fe2f50704c01385767b2ceba
Lazaros Panitsidis <lazarospanitsidis98@gmail.com>
2023-12-16 18:39:36 +0200

    fixed functions and add df_clean

--- functions/__init__.py
+++ functions/__init__.py
@@ -1,4 +1,5 @@
-__all__ = ["optimize_dtypes", "df_info"]
+__all__ = ["optimize_dtypes", "df_info", "df_clean"]
 
 from .data_types import optimize_dtypes
 from .dataframe_actions import df_info
+from .dataframe_actions import df_clean
--- functions/dataframe_actions.py
+++ functions/dataframe_actions.py
@@ -1,3 +1,62 @@
+import pandas as pd
+import numpy as np
+
+def df_info(dataframes, dataframe_names):
+    """
+    Finds useful information about all dataframes given in the function.
+
+    Usage: pass a list of dataframes and their names into the function
+    dataframes = [df1, df2, ...]
+    dataframe_names = ["df1", "df2", ...]
+    """
+
+    for df, df_name in zip(dataframes, dataframe_names):
+        # Check if the dataframe has at least one column
+        if not df.empty:
+            print("----- information for ", df_name, " -----")
+            print(df_name, " : ", df.shape, "(rows, columns)")
+            print(df_name, " : ", df.isna().sum().sum(), "missing values")
+            print(df_name, " : ", df.duplicated().sum(), "duplicated values")
+
+            # Identify and count values of the last column
+            last_column = df.columns[-1]
+            value_counts = df[last_column].value_counts()
+
+            print(df_name, " : Value counts for ", last_column)
+            print(value_counts)
+        else:
+            print(df_name, ': The dataframe is empty.')
+
+
+
+
+def df_clean(df):
+    """
+    Eliminate invalid data from the dataframe.
+
+    This function replaces non-numeric values in the specified columns
+    with NaN and removes rows containing NaN values.
+
+    Parameters:
+    - df: pandas DataFrame
+
+    Returns:
+    - Cleaned DataFrame
+    """
+    df_columns = df.columns.to_list()
+
+    num_df = (
+        df.drop(df_columns, axis=1)
+          .join(df[df_columns].apply(pd.to_numeric, errors='coerce'))
+    )
+
+    num_df = num_df[num_df[df_columns].notnull().all(axis=1)]
+
+    return num_df
+
+
+#### COMMENTS ####
+
 # ### works in notebook as is
 # # dataframe information
 # def df_info(dataframes):
@@ -30,28 +89,28 @@
 #           print(df_name, ': The dataframe is empty.')
 
 
-def df_info(dataframes):
-    """
-    Finds useful information about all dataframes given in the function.
+# def df_info(dataframes):
+#     """
+#     Finds useful information about all dataframes given in the function.
 
-    Usage: pass a list of dataframes into the function
-    dataframes = [df1, df2, ...]
-    """
+#     Usage: pass a list of dataframes into the function
+#     dataframes = [df1, df2, ...]
+#     """
 
-    for i, df in enumerate(dataframes, start=1):
-        df_name = f"df{i}"
-        # Check if the dataframe has at least one column
-        if not df.empty:
-            print("----- information for ", df_name, " -----")
-            print(df_name, " : ", df.shape, "(rows, columns)")
-            print(df_name, " : ", df.isna().sum().sum(), "missing values")
-            print(df_name, " : ", df.duplicated().sum(), "duplicated values")
+#     for df in dataframes:
+#         df_name = f"{df}"
+#         # Check if the dataframe has at least one column
+#         if not df.empty:
+#             print("----- information for ", df_name, " -----")
+#             print(df_name, " : ", df.shape, "(rows, columns)")
+#             print(df_name, " : ", df.isna().sum().sum(), "missing values")
+#             print(df_name, " : ", df.duplicated().sum(), "duplicated values")
 
-            # Identify and count values of the last column
-            last_column = df.columns[-1]
-            value_counts = df[last_column].value_counts()
+#             # Identify and count values of the last column
+#             last_column = df.columns[-1]
+#             value_counts = df[last_column].value_counts()
 
-            print(df_name, " : Value counts for ", last_column)
-            print(value_counts)
-        else:
-            print(df_name, ': The dataframe is empty.')
+#             print(df_name, " : Value counts for ", last_column)
+#             print(value_counts)
+#         else:
+#             print(df_name, ': The dataframe is empty.')
