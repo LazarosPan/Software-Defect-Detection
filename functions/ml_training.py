f7427b4e506ddd4d2365e1cb5626e2b987459470
Lazaros Panitsidis <lazarospanitsidis98@gmail.com>
2023-12-28 13:52:29 +0200

    again

--- functions/ml_training.py
+++ functions/ml_training.py
@@ -41,24 +41,28 @@ def train_classifiers_tuned(classifiers, x, y, cv, search_cv, scoring, param_gri
             # Create a pipeline with only the classifier (no scaling)
             pipe = Pipeline([('classifier', classifier)])
 
-        # GridSearchCV with refit on F1-score
+        # GridSearchCV with refit False
         grid_search = GridSearchCV(pipe, param_grid=param_grid, scoring=scoring,
-                                   cv=search_cv, refit='F1-score', n_jobs=-1)
+                                   cv=search_cv, refit=False, n_jobs=-1)
 
-        # Use cross_validate to obtain scores
-        scores = cross_validate(grid_search, x, y, cv=cv, scoring=scoring, n_jobs=-1, return_train_score=False)
+        # Perform grid search
+        grid_search.fit(x, y)
 
-        # Obtain the best parameters and best estimator from the refit attribute
-        best_params = grid_search.best_params_
-        best_estimator = grid_search.refit
+        # Get the best estimator
+        best_estimator = grid_search.best_estimator_
+
+        # Manually refit the best estimator on the entire training data on the F1 score
+        best_estimator.fit(x, y, scoring='f1_score')
+
+        # Calculate the F1 score of the manually refit model
+        scores = cross_validate(best_estimator, x, y, cv=cv, scoring=scoring, n_jobs=-1, return_train_score=False)
+        manual_f1 = stats.fmean(scores['test_F1-score'])
 
         results[classifier.__class__.__name__] = {
             'Accuracy': stats.fmean(scores['test_Accuracy']),
-            'F1-score': stats.fmean(scores['test_F1-score']),
+            'F1-score': manual_f1,
             'G-Mean score': stats.fmean(scores['test_G-Mean score']),
-            'Fit time': sum(scores['fit_time']),
-            'Best Parameters': best_params,
-            'Best Estimator': best_estimator
+            'Fit time': sum(scores['fit_time'])
         }
 
     return results
