0a41638e9430f2a0895987df323f2ab1c420a1b7
Lazaros Panitsidis <lazarospanitsidis98@gmail.com>
2023-12-28 13:30:06 +0200

    testing train function with tuing in new notebook

--- functions/__init__.py
+++ functions/__init__.py
@@ -1,6 +1,7 @@
-__all__ = ["optimize_dtypes", "df_info", "df_clean", "train_classifiers"]
+__all__ = ["optimize_dtypes", "df_info", "df_clean", "train_classifiers", "train_classifiers_tuned"]
 
 from .data_types import optimize_dtypes
 from .dataframe_actions import df_info
 from .dataframe_actions import df_clean
 from .ml_training import train_classifiers
+from .ml_training import train_classifiers_tuned
--- functions/ml_training.py
+++ functions/ml_training.py
@@ -2,6 +2,8 @@
 from sklearn.pipeline import Pipeline
 from sklearn.model_selection import cross_validate
 import statistics as stats
+from sklearn.model_selection import GridSearchCV, RandomizedSearchCV
+from sklearn.metrics import make_scorer, f1_score
 
 def train_classifiers(classifiers, x, y, cv, scoring, scaler=None):
     results = {}
@@ -23,6 +25,77 @@ def train_classifiers(classifiers, x, y, cv, scoring, scaler=None):
             'F1-score': stats.fmean(scores['test_F1-score']),
             'G-Mean score': stats.fmean(scores['test_G-Mean score']),
             'Fit time': sum(scores['fit_time'])
+        }
+
+    return results
+
+
+# def train_classifiers_tuned(classifiers, x, y, cv, search_cv, scoring, param_grid, scaler=None):
+#     results = {}
+
+#     for classifier in classifiers:
+#         if scaler is not None:
+#             # Create a pipeline with the specified scaler and the current classifier
+#             pipe = Pipeline([('scaler', scaler), ('classifier', classifier)])
+#         else:
+#             # Create a pipeline with only the classifier (no scaling)
+#             pipe = Pipeline([('classifier', classifier)])
+
+#         # GridSearchCV with refit on F1-score
+#         grid_search = GridSearchCV(pipe, param_grid=param_grid, scoring=scoring,
+#                                    cv=search_cv, refit='f1_score', n_jobs=-1)
+
+
+
+#         # Use cross_validate to obtain scores
+#         scores = cross_validate(grid_search, x, y, cv=cv, scoring=scoring, n_jobs=-1, return_train_score=False)
+
+#         results[classifier.__class__.__name__] = {
+#             'Accuracy': stats.fmean(scores['test_Accuracy']),
+#             'F1-score': stats.fmean(scores['test_F1-score']),
+#             'G-Mean score': stats.fmean(scores['test_G-Mean score']),
+#             'Fit time': sum(scores['fit_time'])
+#         }
+
+#     return results
+
+
+def train_classifiers_tuned(classifiers, x, y, cv, search_cv, scoring, param_grid, scaler=None):
+    results = {}
+
+    for classifier in classifiers:
+        if scaler is not None:
+            # Create a pipeline with the specified scaler and the current classifier
+            pipe = Pipeline([('scaler', scaler), ('classifier', classifier)])
+        else:
+            # Create a pipeline with only the classifier (no scaling)
+            pipe = Pipeline([('classifier', classifier)])
+
+        # GridSearchCV with refit False
+        grid_search = GridSearchCV(pipe, param_grid=param_grid, scoring=scoring,
+                                   cv=search_cv, refit=False, n_jobs=-1)
+
+        # Perform grid search
+        grid_search.fit(x, y)
+
+        # Get the best estimator
+        best_estimator = grid_search.best_estimator_
+
+        # Manually refit the best estimator on the entire training data
+        best_estimator.fit(x, y)
+
+        # Get the best parameters
+        best_params = grid_search.best_params_
+
+        # Use cross_validate to obtain scores
+        scores = cross_validate(best_estimator, x, y, cv=cv, scoring=scoring, n_jobs=-1, return_train_score=False)
+
+        results[classifier.__class__.__name__] = {
+            'Accuracy': stats.fmean(scores['test_Accuracy']),
+            'F1-score': stats.fmean(scores['test_F1-score']),
+            'G-Mean score': stats.fmean(scores['test_G-Mean score']),
+            'Fit time': sum(scores['fit_time']),
+            'Best Parameters': best_params
         }
 
     return results
